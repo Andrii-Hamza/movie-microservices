7769645ba23524dc81bf2b51923c2e0e41e27d1d
Andrii <stasaaass@gmail.com>
2025-09-18 06:02:01 +0200

    Distributed Tracing

--- order-service/src/main/java/com/programming/order_service/service/OrderService.java
+++ order-service/src/main/java/com/programming/order_service/service/OrderService.java
@@ -1,12 +1,16 @@
 package com.programming.order_service.service;
 
+import brave.ScopedSpan;
+import brave.Tracer;
 import com.programming.order_service.dto.InventoryResponse;
 import com.programming.order_service.dto.OrderLineItemsDto;
 import com.programming.order_service.dto.OrderRequest;
 import com.programming.order_service.model.Order;
 import com.programming.order_service.model.OrderLineItems;
 import com.programming.order_service.repo.OrderRepository;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -19,11 +23,14 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final Tracer tracer;
 
+    @TimeLimiter(name = "inventory")
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -31,34 +38,41 @@ public class OrderService {
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
                 .stream()
                 .map(this::mapToDto)
-                // .toList();
                 .collect(Collectors.toList());
-
 
         order.setOrderLineItemsList(orderLineItems);
 
         List<String> skuCodes = order.getOrderLineItemsList().stream()
                 .map(OrderLineItems::getSkuCode)
-                //.toList();
                 .collect(Collectors.toList());
 
-        // Call Inventory Service, and place order if movie is in stock
-        InventoryResponse[] inventoryResponsArray = webClientBuilder.build().get()
-                .uri("http://inventory-service/api/inventory",
-                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes)
-                                .build())
-                        .retrieve()
-                                .bodyToMono(InventoryResponse[].class)
-                                        .block();
+        ScopedSpan inventoryServiceLookup = tracer.startScopedSpan("InventoryServiceLookup");
 
-        boolean allProductsInStock = Arrays.stream(inventoryResponsArray)
-                .allMatch(InventoryResponse::isInStock);
+        try {
+            log.info("Checking inventory for skuCodes: {}", skuCodes);
+            InventoryResponse[] inventoryResponsArray = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes)
+                                    .build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
 
-        if(allProductsInStock) {
-            orderRepository.save(order);
-            return "Order Placed successfully";
-        } else {
-            throw new IllegalArgumentException("Order is not in stock, try later");
+            boolean allProductsInStock = Arrays.stream(inventoryResponsArray)
+                    .allMatch(InventoryResponse::isInStock);
+
+            if (allProductsInStock) {
+                orderRepository.save(order);
+                log.info("Order {} placed successfully", order.getOrderNumber());
+                return "Order Placed successfully";
+            } else {
+                throw new IllegalArgumentException("Product is not in stock, please try again later.");
+            }
+        } catch (Exception e) {
+            inventoryServiceLookup.error(e);
+            throw e;
+        } finally {
+            inventoryServiceLookup.finish();
         }
     }
 
